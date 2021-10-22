224e0334d62f097255f6109e3f1d0b49849f051c
Ekaterina Kalashnikova <mochalova_ekaterina@lenta.ru>
2021-10-22 16:16:31 +0300

    checkNum

--- unikNumber.js
+++ unikNumber.js
@@ -1,37 +1,35 @@
+function checkNum (n) {
+  if (checkInRow (board, n), checkInColumn(board, n), checkInBox(board, n))
+  return true 
+}
 
 let sudokuLength = board[0].length
 let boxSize = Math.sqrt(sudokuLength)
 
 
-function checkInRow (array, num) {
+function checkInRow (array, n) {
   for (let i = 0; i < sudokuLength; i++) {
-    if (array[i][c] == num && i !== r) return false
+    if (array[i][c] == n && i !== r) return false
   }
   return true
 }
 
-function checkInColumn (array, num) {
+function checkInColumn (array, n) {
   for (let i = 0; i < sudokuLength; i++) {
-    if (array[r][i] == num && i !== c) return false
+    if (array[r][i] == n && i !== c) return false
   }
   return true
 }
-
-
-//нужно вычислить координату верхнего угла бокса и от неё вычислить с какого по какой индесы проходиться по ячейке 
 
 let boxCornerRow = Math.floor(r/boxSize) * boxSize
 let boxCornerColumn = Math.floor(c/boxSize) * boxSize
 
 
-// вычисление координаты работает
-
-function checkInBox(array, num) {
+function checkInBox(array, n) {
   for (let i = boxCornerRow ; i < boxCornerRow + boxSize; i++) {
     for (let j = boxCornerColumn; j < boxCornerColumn + boxSize; j++) {
-      if (array[i][j] === num && i !== r && j !== c) {return false}
+      if (array[i][j] === n && i !== r && j !== c) {return false}
     }    
   }
   return true  
 }
-
