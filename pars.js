9780eb5d74b9997f74aaad6e9571e88793c121ae
Alexandr Ryazanov <dockannes@gmail.com>
2021-10-22 14:45:52 +0300

    Представление доски в виде многомерного массива

--- pars.js
+++ pars.js
@@ -1,22 +1,9 @@
 const fs = require('fs')
 
-let string = fs.readFileSync('./sudoku-puzzles.txt', 'utf8').split('\n')[0]
+let string = fs.readFileSync('./sudoku-puzzles.txt', 'utf8').split('\n')[0].replace(/-/gi, '0')
 
 const board = []
 
-
-let stringOneChange = string.replace(/-/gi, '0')
-console.log(stringOneChange);
-
-
-for (let i = 0; i < stringOneChange.length; i += 9) {
-  board.push(stringOneChange.slice(i, i + 9).split('').map(Number))
+for (let i = 0; i < string.length; i += 9) {
+  board.push(string.slice(i, i + 9).split('').map(Number))
 }
-
-console.log(board)
-
-
-
-
-
-
