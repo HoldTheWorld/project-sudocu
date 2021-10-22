402f42d2195ed6e68d76bd82df123553abbcbce2
Ekaterina Kalashnikova <mochalova_ekaterina@lenta.ru>
2021-10-22 15:51:06 +0300

    функции для проверки строчек, колонок и ячеек 3х3

--- unikNumber.js
+++ unikNumber.js
@@ -1,30 +1,48 @@
 let mySudoku = [
-  [0, 4, 0, 2],
-  [3, 0, 1, 4],
-  [2, 1, 0 ,0],
-  [0, 3, 2, 1]  
-]
+  [2, 1, 9, 0, 4, 6, 0 ,3, 0],
+  [0, 0, 5, 1, 0, 0, 0, 0, 0],
+  [0, 3, 4, 0, 0, 0, 2, 6, 0],
+  [0, 2, 6, 0, 0, 7, 5, 0, 3],
+  [0, 0, 1, 0, 9, 0, 0, 0, 7],
+  [4, 7, 3, 0, 6, 5, 0, 0, 8],
+  [0, 6, 0, 4, 0, 2, 3, 1, 0],
+  [3, 4, 0, 0, 0, 0, 7, 8, 0],
+  [1, 0, 0, 0, 0, 0, 4, 5, 0]
+];
 let sudokuLength = mySudoku[0].length
 let boxSize = Math.sqrt(sudokuLength)
 
-function checkInColumn (array, num) {
-  for (let i = 0; i < sudokuLength; i++) {
-    if (num == mySudoku[i][c] && i !== c) return false
-  }
-  return true
-}
 
 function checkInRow (array, num) {
   for (let i = 0; i < sudokuLength; i++) {
-    if (num == mySudoku[r][i] && i !== r) return false
+    if (mySudoku[i][c] == num && i !== r) return false
+  }
+  return true
+}
+
+function checkInColumn (array, num) {
+  for (let i = 0; i < sudokuLength; i++) {
+    if (mySudoku[r][i] == num && i !== c) return false
   }
   return true
 }
 
 
-// //нужно вычислить координату верхнего угла бокса и от неё вычислить с какого по какой индесы проходиться по ячейке 
+//нужно вычислить координату верхнего угла бокса и от неё вычислить с какого по какой индесы проходиться по ячейке 
 
-// let boxCornerColumn = Math.floor(c/boxSize) * boxSize
-// let boxCornerRow = Math.floor(r/boxSize) * boxSize
+let boxCornerRow = Math.floor(r/boxSize) * boxSize
+let boxCornerColumn = Math.floor(c/boxSize) * boxSize
 
-// // вычисление координаты работает
+
+// вычисление координаты работает
+
+function checkInBox(array, num) {
+  for (let i = boxCornerRow ; i < boxCornerRow + boxSize; i++) {
+    for (let j = boxCornerColumn; j < boxCornerColumn + boxSize; j++) {
+      if (mySudoku[i][j] === num && i !== r && j !== c) return false
+    }    
+  }
+  return true  
+}
+
+
